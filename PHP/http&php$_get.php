9119297273de166e55a6f7df468cb7737fcdb65d
Saja <91623920+Watch-Me-Fly@users.noreply.github.com>
2022-05-29 21:43:18 +0200

    $_get

---
+++ PHP/http&php$_get.php
@@ -0,0 +1,25 @@
+<!-- <?php
+?> -->
+
+<h1>Contents of $_GET array</h1>
+<p>using print_r:</p>
+<pre>
+    <?php
+        print_r($_GET);
+    ?>
+</pre>
+<p>using var_dump:</p>
+<pre>
+    <?php
+        var_dump($_GET);
+    ?>
+</pre>
+
+<!-- add : (?x=2&y=4) to URL -->
+
+<!-- $_GET is a super array, 
+    it includes what is after ?, 
+    add another parameter by adding & 
+-->
+
+<!-- shuffle()  is used to rearrange an array in random order. -->
