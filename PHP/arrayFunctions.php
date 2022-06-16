d3ab08a0296dfb46145e3f3a8018e3642168510f
Saja <91623920+Watch-Me-Fly@users.noreply.github.com>
2022-06-16 22:48:35 +0200

    array functions

--- PHP/arrayFunctions.php
+++ PHP/arrayFunctions.php
@@ -4,97 +4,115 @@
     $value = "";
     $array["key"] = $value;
 
-    array_key_exists("key", $array);
-    // returns true if key is set in the array
+    $example = array(
+        "name" => "chuck",
+        "course" => "PHP"
+    );
 
-    isset($array['key']);
-    // returns true if key is set in the array too
+    /**============================================
+     *               SECTION functions
+     *=============================================**/
+        /* ----------------------------------------------------
+            Check if the given key/index exists in the array (boolean) 
+            * array_key_exists("key", $array);
+        ---------------------------------------------------- */
+            if ( array_key_exists('course', $example) )
+            {
+                echo "course key exists </br>";
+            } else {
+                echo "course key not found</br>";
+            }
+            echo "<br/>";
 
-    count($array);
-    // how many elements in the array 
-
-    is_array($array);
-    // returns true if a variable exists in the array
-
-    sort($array);
-    // sorts the array values (loses key)
-
-    ksort($array);
-    // sorts the array values by key
-
-    asort($array);
-    // sorts the array by value, keeping key association
+        /* ----------------------------------------------------
+            Determine if a variable is declared and is different than NULL (boolean)
+            * isset($array['key']);
+        ---------------------------------------------------- */
+            // php < 7.0.0 
+            echo isset($example["name"]) ?
+                "name is set<br/>" : "name is not set<br/>";
+            echo isset($example['addr']) ? 
+                "address is set\n <br/>" : "address is not set\n <br/>";
+            echo "<br/>";
     
-    shuffle($array);
-    // shuffles the array into random order
+            /**----------------------------------------------
+             **                     NOTE
+                      ---------------------------------
+             * isset + (? :) is equivalent to :
+                //* "null coalescing operator (??)"
+             *---------------------------------------------**/
+            $name = $example["name"] ?? "not found";
+            echo "name = " . $name;
+        
+        /* ----------------------------------------------------
+            how many elements in the array
+            * count($array);
+        ---------------------------------------------------- */
+            print "count : " . count($example) . "<br/>";
+            echo "<br/>"."<br/>";
+        /* ----------------------------------------------------
+            returns true if a variable exists in the array
+            * is_array($array);
+        ---------------------------------------------------- */
+            if ( is_array($example) )
+            {
+                echo "example is an array <br/>";
+            } else {
+                echo "example is not an array <br/>";
+            }
 
-
-    /***********************************/
-    $ar = array();
-    $ar["name"] = "chuck";
-    $ar["course"] = "php";
-
-    if (array_key_exists('course', $ar)) {
-        echo ("course exists\n <br/>");
-    } else {
-        echo ("course not found\n <br/>");
-    }
-
-    echo "<br/>";
-
-    // php < 7.0.0 
-    echo isset($ar['name']) ? 
-        "name is set\n <br/>": "name is not set\n <br/>";
-    echo isset($ar['addr']) ? 
-        "address is set\n <br/>" : "address is not set\n <br/>";
-
-    echo "<br/>";
-
-        /*
-            equivalent to the following "null coalescing operator (??)" 
-                (php >= 7.0.0)
-        */
-        $name = $ar["name"] ?? 'not found';
-        echo "name = $name";
-
-
-    echo "<br/>"."<br/>";
-    
-    print "count: " . count($ar) . "<br/>";
-    if ( is_array($ar) ) {
-        echo '$ar is an array'; // signle quote will not print as var
-    } else {
-        echo "$ar is not an array"; // double quotes considered var
-    }
-
-    /*
-        sort() -- works with literal array but not key value arrays
-            sort($ar); 
+        /* ----------------------------------------------------
+            sorts the array values (loses key)
+            * sort($array);
+        ---------------------------------------------------- */
+            /**----------------------------------------------
+             **                     NOTE
+                      ---------------------------------
+             * //* sort() :
+             *      works with literal array 
+             *      does not work key value arrays
+             *---------------------------------------------**/
+            sort($example);
             echo "<pre>";
-            print_r($ar);
-            echo "</pre>";
+            print_r($example);
+            echo "</pre> <br/>";
 
-        ksort() -- sorts by key
-            ksort($ar);
+        /* ----------------------------------------------------
+            sorts the array values by key
+            * ksort($array);
+        ---------------------------------------------------- */
+            ksort($example);
             echo "<pre>";
-            print_r($ar);
-            echo "</pre>";
-    */
+            print_r($example);
+            echo "</pre> <br/>";
+        /* ---------------------------------------------------- 
+            sorts the array by value, keeping key association in place
+            * asort($array);
+        ---------------------------------------------------- */
+            asort($example);
+            echo "<pre>";
+            print_r($example);
+            echo "</pre> <br/>";
+        /* ----------------------------------------------------
+            shuffles the array into random order
+            * shuffle($array);
+        ---------------------------------------------------- */
+            shuffle($example);
+            echo "<pre>";
+            print_r($example);
+            echo "</pre> <br/>";
+        /* ----------------------------------------------------
+            exploding arrays & string processing
+             (it makes an array from a string)
+            * explode($separator, $string, $limit ?);
+        ---------------------------------------------------- */
+            $sentence = "This is a sentence with seven words";
+            $exploded = explode(' ', $sentence);
+                echo "<pre>";
+                print_r($exploded);
+                echo "</pre> <br/>";
+            $verifyExplosion = is_array($exploded) ?
+                    "Yes" : "No";
+                echo $verifyExplosion;
 
-    // asort() -- sorts by value, keeping keys in place
-    asort($ar); 
-    echo "<pre>";
-    print_r($ar);
-    echo "</pre>";
-
-    // exploding arrays & string processing 
-        // (it makes an array from string)
-    $inp = "This is a sentence with seven words";
-    $temp = explode(' ', $inp); // splitting spaces (can be ,/)
-    echo "<pre>";
-    print_r($temp);
-    echo "</pre>";
-
-    $verif = is_array($temp) ? "yes" : "no";
-    echo $verif;
 ?>
